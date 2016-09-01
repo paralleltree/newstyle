eec90d5e3a83b7d366b0b19ebcf4ca0deac2440f
Kenta Yoshikawa <me@kenta.cc>
2016-09-01 22:05:02 +0900

    Connected the two database with relationship !

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,2 +1,5 @@
 class Content < ActiveRecord::Base
+  has_many :relationship, :class_name => 'Relationship', :foreign_key => 'a_id', :dependent => :destroy
+  has_many :relationship, :class_name => 'Relationship', :foreign_key => 'b_id', :dependent => :destroy
+
 end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,4 @@
 class Relationship < ActiveRecord::Base
+  belogs_to :a_id, :class_name => 'Content'
+  belogs_to :b_id, :class_name => 'Content'
 end

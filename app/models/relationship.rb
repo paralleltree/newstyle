519980f24c4f6b73d15fc2c779496548b6258125
Kenta Yoshikawa <me@kenta.cc>
2016-09-01 22:28:55 +0900

    Fix the spellmiss belogs to "belongs"

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,4 @@
 class Relationship < ActiveRecord::Base
-  belogs_to :a_id, :class_name => 'Content'
-  belogs_to :b_id, :class_name => 'Content'
+  belongs_to :a_id, :class_name => 'Content'
+  belongs_to :b_id, :class_name => 'Content'
 end

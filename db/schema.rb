1d1b2771380571e99c19f0f767c352db78646bd9
Kazuto Tsunoda <color.ui>
2016-09-02 10:11:53 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160901075633) do
+
+  create_table "contents", force: :cascade do |t|
+    t.text     "title"
+    t.text     "url"
+    t.text     "summary"
+    t.text     "body"
+    t.text     "keyword"
+    t.text     "trend"
+    t.datetime "time"
+    t.text     "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

1e32edf7810b3b260a07f2be62f38feeec6a4bb0
Kazuto Tsunoda <color.ui>
2016-09-02 15:33:25 +0900

    nil test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Content.create(title: 'test news',url: 'http://www.example.com',summary: 'summary_text',body: 'body_text',keyword: "key_text",trend: 'news',time: '1000-01-01 00:00:00',author: 'NIT,Oyama')
+#Content.create(title: 'test news',url: 'http://www.example.com',summary: 'summary_text',body: 'body_text',keyword: "key_text",trend: 'news',time: '1000-01-01 00:00:00',author: 'NIT,Oyama')
+
+Content.create(title: 'test news',url: 'http://www.example.com',summary: 'summary_text',trend: 'news',time: '1000-01-01 00:00:00',author: 'NIT,Oyama')
